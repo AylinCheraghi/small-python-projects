229127ae4819f62c697e0994124c66a8545adfc2
Aylin Cheraghi <ayaylinay06@gmail.com>
2026-06-29 18:28:31 +0330

    description of game

---
+++ Cows_and_Bulls_game.py
@@ -0,0 +1,4 @@
+#Cows and Bulls is a code-breaking game that tests logical thinking and deduction skills. 
+#In this game, the computer generates a secret 4-digit number with unique digits, and the
+#player attempts to guess it. After each guess, the program provides hints in the form of
+#bulls and cows, helping the player narrow down the correct number.
