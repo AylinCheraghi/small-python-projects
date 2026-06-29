dce773340e45d204312bcc3f1dfac15c83ac6240
Aylin Cheraghi <ayaylinay06@gmail.com>
2026-06-29 18:22:38 +0330

    Last changes of emoji to text file

--- Emoji_To_Text.py
+++ Emoji_To_Text.py
@@ -1,5 +1,13 @@
 #remove emojis from text
+
 import demoji
+demoji.download_codes()
+
 text = "Good Morning! 🌸🌷"
 a1 = demoji.replace(text, "")
+print(a1)
+
+#Replace Emojis with Descriptive Text
+text = "Happy birthday! 🎂🎈"
+a1 = demoji.replace(text)
 print(a1)
