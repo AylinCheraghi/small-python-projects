32bb55b74180f501fa7f5bd698bbf4f8c9be4271
Aylin Cheraghi <ayaylinay06@gmail.com>
2026-06-29 19:06:03 +0330

    just first line of code

--- Cows_and_Bulls_game.py
+++ Cows_and_Bulls_game.py
@@ -2,3 +2,9 @@
 #In this game, the computer generates a secret 4-digit number with unique digits, and the
 #player attempts to guess it. After each guess, the program provides hints in the form of
 #bulls and cows, helping the player narrow down the correct number.
+
+import random
+
+
+print("Enter of tries: ")
+tries = int(input())
