8800213e2360ede45b914ea06146ebaf5a47cde0
Aylin Cheraghi <ayaylinay06@gmail.com>
2026-06-30 22:24:15 +0330

    wrote functions

--- Cows_and_Bulls_game.py
+++ Cows_and_Bulls_game.py
@@ -5,6 +5,29 @@
 
 import random
 
+def getDigits(num):
+    return[int(i) for i in str(num)]
+
+def noDuplicates(num):
+    num_li = getDigits(num)
+    if len(num_li)  == len(set(num_li)):
+       return True
+    else:
+       return False
+
+def numOfBullsCows (num, guess):
+    bull_cow = [0,0]
+    num_li = getDigits(num)
+    guess_li = getDigits(guess)
+     
+    for i,j in zip(num_li, guess_li):
+        if j in num_li:
+            if j == i:
+                bull_cow[0] += 1
+            else:
+                bull_cow[1] +=1
+        return bull_cow
+        
 
 print("Enter of tries: ")
 tries = int(input())
